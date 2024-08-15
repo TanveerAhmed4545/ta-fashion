f2345e619fccf93752de63f4c7ead00ed24f2906
TanveerAhmed4545 <tanveer8507@gmail.com>
2024-08-15 21:59:18 +0600

    add search and products card

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,55 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
+import ProductsCard from "./ProductsCard/ProductsCard";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [search, setSearch] = useState("");
+
+  console.log(search);
+  const {
+    data: products = [],
+    // isLoading,
+    // error,
+  } = useQuery({
+    queryKey: ["products", search],
+    queryFn: async () => {
+      const res = await axios.get(`http://localhost:5000/Products`, {
+        params: {
+          search,
+        },
+      });
+      return res.data;
+    },
+  });
+  // console.log(products);
+
+  return (
+    <div>
+      <div className="text-center py-8 ">
+        <h2 className=" text-2xl lg:text-4xl font-extrabold">
+          Discover our Latest Fashion
+        </h2>
+        <p className=" pt-6 w-full mx-auto lg:w-9/12">
+          Explore the latest in Fashion on our platform.{" "}
+        </p>
+      </div>
+      <div className="my-3 px-4">
+        <input
+          className="input input-bordered mb-3 w-full"
+          type="text"
+          placeholder="Search by ProductName or BrandName"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-5 px-4">
+        {products.map((product) => (
+          <ProductsCard key={product._id} product={product}></ProductsCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/pages/Home/ProductsCard/ProductsCard.jsx
@@ -0,0 +1,67 @@
+/* eslint-disable react/prop-types */
+import moment from "moment";
+import { BiCategory } from "react-icons/bi";
+import { FaClock } from "react-icons/fa";
+import { HiCurrencyDollar } from "react-icons/hi";
+import { IoMdStar } from "react-icons/io";
+
+const ProductsCard = ({ product }) => {
+  const {
+    CreationDateTime,
+    Description,
+    ProductImage,
+    ProductName,
+    Price,
+    Ratings,
+    Category,
+  } = product;
+
+  // Format the CreationDateTime using Moment.js
+  const formattedDateTime = moment(CreationDateTime).format(
+    "MMMM Do YYYY, h:mm A"
+  );
+  return (
+    <div>
+      <div className="card  p-4 bg-base-100 border-[#e7f6fd] border-2 hover:border-none hover:shadow-xl hover:shadow-[#e7f6fd] transition duration-300 ease-in-out">
+        <div className="relative">
+          <figure>
+            <img
+              className="h-80 md:h-96 lg:h-96 w-full rounded-xl"
+              src={ProductImage}
+            />
+          </figure>
+          <button className="absolute top-3 right-3 flex justify-center gap-1 items-center  px-2 py-1 text-white rounded-xl bg-[#0EB1EA]">
+            {Ratings}
+            <IoMdStar />
+          </button>
+        </div>
+        <div className="mt-3">
+          <h2 className="card-title hover:text-[#0EB1EA]">{ProductName}</h2>
+          <p className="flex items-center my-3">
+            <span className="text-[#0EB1EA] mr-2 text-lg ">
+              <BiCategory />
+            </span>{" "}
+            <span className="font-semibold">{Category}</span>
+          </p>
+          <div className="flex items-center justify-between">
+            <p className="flex items-center gap-2 text-xs md:text-base font-semibold">
+              <span>
+                <FaClock className="text-[#0EB1EA] " />
+              </span>
+              {formattedDateTime}
+            </p>
+            <p className="flex justify-center items-center text-xs md:text-base  gap-2 font-semibold">
+              <HiCurrencyDollar className="text-[#0EB1EA] text-base md:text-lg" />
+              {Price}
+            </p>
+          </div>
+          <p className="my-3 font-semibold hover:text-[#0EB1EA]">
+            {Description}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsCard;
