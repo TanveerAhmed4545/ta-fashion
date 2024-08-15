cc6aeab5c4f1a7b22de5d2e90b85250e85692c17
TanveerAhmed4545 <tanveer8507@gmail.com>
2024-08-15 22:04:09 +0600

    add pagination

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,24 +5,54 @@ import ProductsCard from "./ProductsCard/ProductsCard";
 
 const Home = () => {
   const [search, setSearch] = useState("");
-
+  const [currentPage, setCurrentPage] = useState(0);
   console.log(search);
   const {
     data: products = [],
     // isLoading,
     // error,
   } = useQuery({
-    queryKey: ["products", search],
+    queryKey: ["products", search, currentPage],
     queryFn: async () => {
-      const res = await axios.get(`http://localhost:5000/Products`, {
-        params: {
-          search,
-        },
-      });
+      const res = await axios.get(
+        `http://localhost:5000/Products?page=${currentPage}`,
+        {
+          params: {
+            search,
+          },
+        }
+      );
       return res.data;
     },
   });
   // console.log(products);
+
+  const { data: totalCount = {} } = useQuery({
+    queryKey: ["userCount"],
+    queryFn: async () => {
+      const res = await axios.get("http://localhost:5000/productCount");
+      return res.data;
+    },
+  });
+
+  const { count } = totalCount;
+  const itemsPerPage = 9;
+
+  const numberOfPages = Math.ceil(count / itemsPerPage);
+  const pages = numberOfPages > 0 ? [...Array(numberOfPages).keys()] : [];
+  // console.log(count,itemsPerPage,numberOfPages,pages);
+
+  const handlePrevPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < pages.length - 1) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
   return (
     <div>
@@ -47,6 +77,27 @@ const Home = () => {
         {products.map((product) => (
           <ProductsCard key={product._id} product={product}></ProductsCard>
         ))}
+      </div>
+      <div className="text-center py-5 mt-auto ">
+        <button onClick={handlePrevPage} className=" btn btn-md mr-1">
+          « Prev
+        </button>
+        {pages.map((page, idx) => (
+          <button
+            onClick={() => setCurrentPage(page)}
+            key={idx}
+            className={
+              currentPage === page
+                ? "btn btn-md btn-active mr-1"
+                : "btn  btn-md mr-1"
+            }
+          >
+            {page + 1}
+          </button>
+        ))}
+        <button onClick={handleNextPage} className=" btn btn-md mr-1">
+          Next »
+        </button>
       </div>
     </div>
   );
